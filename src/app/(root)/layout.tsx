20aa79cf0e96861899c3ca65597d56a789ebca10
GeorgeJose <subingeorge027@gmail.com>
2024-01-29 23:04:06 +0530

    adds authentication

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/src/components/shared/Footer";
+import Header from "@/src/components/shared/Header";
+
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <div className="flex flex-col h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    );
+  }
---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { RedirectToSignIn, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src={"/assets/images/logo.svg"}
+            alt={"evently logo"}
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
